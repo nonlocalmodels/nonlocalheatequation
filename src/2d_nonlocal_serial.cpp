6a28526bfb8f875b81cb4536d78801a195850d3e
Pranavug <pranav.gayu@gmail.com>
2020-06-10 17:10:54 +0530

    Adding test for 1d and 2d

--- src/2d_nonlocal_serial.cpp
+++ src/2d_nonlocal_serial.cpp
@@ -18,7 +18,11 @@
 
 #include "print_time_results.hpp"
 
+//mathematical constant PI
+#define PI 3.14159265
+
 bool header = 1;
+double k = 1 ;     // heat transfer coefficient
 double dt = 1.;     // time step
 double dh = 1.;     // grid spacing
 
@@ -42,8 +46,9 @@ public:
     //ny = length of grid in y dimension
     //nt = number of timesteps
     //eps = Epsilon for influence zone of a point
-    long nx, ny, nt, eps;
-    bool current, next;
+    long nx, ny, nt, eps, c_2d;
+    bool current, next, test;
+    double error;
 
     //constructor to initialize class variables and allocate
     solver(long nx, long ny, long nt, long eps)
@@ -52,7 +57,10 @@ public:
         this->ny = ny;
         this->nt = nt;
         this->eps = eps;
+        this->c_2d = (k * 8)/ pow(eps * dh, 4);
         this->current = 0;
+        this->error = 0.0;
+        this->test = 0;
 
         for(auto &sx : S)
         {
@@ -64,9 +72,21 @@ public:
         }
     }
 
-    //init for testing the 1d nonlocal equation
-    void init_space()
+    //print error for testing
+    void print_error(bool cmp)
     {
+        std::cout << error << std::endl;
+        if(cmp)
+            for(long i = 0; i < nx; ++i)
+                for(long j = 0; j < ny; ++j)
+                    std::cout << "Expected: " << w(i, j, nt)
+                    << " Actual: " << S[nt % 2][i][j] << std::endl;
+    }
+
+    //input the initialization for 2d nonlocal equation
+    void input_init()
+    {
+        test = 0;
         for(auto &sx : S[0])
         {
             for(auto &sy : sx)
@@ -76,14 +96,16 @@ public:
         }
     }
 
-    //default init for space to check correctness
-    void default_init()
+    //init for testing the 2d nonlocal equation
+    void test_init()
     {
+        test = 1;
         for(long i = 0; i < nx; ++i)
         {
             for(long j = 0; j < ny; ++j)
             {
-                S[0][i][j] = i;
+                S[0][i][j] = sin(2 * PI * (i * dh))
+                    *sin(2 * PI * (j * dh));
             }
         }
     }
@@ -92,6 +114,26 @@ public:
     static double influence_function(double distance)
     {
         return 1.0;
+    }
+
+    //testing operator to verify correctness
+    inline double w(long pos_x, long pos_y, long time)
+    {
+        return cos(2 * PI * (time * dt)) 
+                * sin(2 * PI * (pos_x * dh))
+                * sin(2 * PI * (pos_y * dh));
+    }
+
+    //condition to enforce the boundary conditions
+    inline double boundary(long pos_x, long pos_y, double val = 2.0)
+    {
+        if(pos_x >= 0 && pos_x < nx && pos_y >= 0 && pos_y < ny)
+            if(val != 2.0)
+                return val;
+            else
+                return S[current][pos_x][pos_y];
+        else
+            return 0;
     }
 
     // Function to find distance in 2d plane given 2 points coordinates
@@ -108,25 +150,50 @@ public:
         return sqrt((eps * eps) - (len_x * len_x));
     }
 
+    //Following function adds the external source to the 2d nonlocal heat equation
+    double sum_local_test(long pos_x, long pos_y, long time)
+    {
+        double result_local = - (2 * PI * sin(2 * PI * (time * dt)) 
+                                * sin(2 * PI * (pos_x * dh))
+                                * sin(2 * PI * (pos_y * dh)));
+        double w_position = w(pos_x, pos_y, time);
+        long len_line = 0;
+
+        for(long i = pos_x-eps; i <= pos_x+eps; ++i)
+        {
+            len_line = len_1d_line(std::abs((long)pos_x - (long)i));
+            for(long j = pos_y-len_line; j <= pos_y+len_line; ++j)
+            {
+                result_local -= influence_function(distance(pos_x, pos_y, i, j))
+                                * c_2d
+                                * (boundary(i, j, w(i, j, time)) - w_position)
+                                * (dh * dh);
+            }
+        }
+        
+        return result_local;
+    }
+
     // Our operator to find sum of 'eps' radius circle in vicinity of point P(x,y)
     // Represent circle as a series of horizaontal lines of thickness 'dh'
     double sum_local(long pos_x, long pos_y)
     {
-        double temp = 0.0;
+        double result_local = 0.0;
         long len_line = 0;
 
-        for(long i = std::max((long)0,pos_x-eps); i <= std::min(pos_x+eps,nx-1); ++i)
+        for(long i = pos_x-eps; i <= pos_x+eps; ++i)
         {
             len_line = len_1d_line(std::abs((long)pos_x - (long)i));
-            for(long j = std::max((long)0,pos_y-len_line); j <= std::min(pos_y+len_line,ny-1); ++j)
+            for(long j = pos_y-len_line; j <= pos_y+len_line; ++j)
             {
-                temp += influence_function(distance(pos_x, pos_y, i, j))
-                                * (S[current][i][j] - S[current][pos_x][pos_y]);
+                result_local += influence_function(distance(pos_x, pos_y, i, j))
+                                * c_2d
+                                * (boundary(i, j) - S[current][pos_x][pos_y])
+                                * (dh * dh);
             }
         }
         
-        temp *= ((1 / (pow(eps*dh,4))) * (dh * dh));
-        return temp;
+        return result_local;
     }
 
     // do all the work on 'nx * ny' data points for 'nt' time steps
@@ -143,16 +210,57 @@ public:
                 for (long j = 0; j < ny; ++j)
                 {
                     S[next][i][j] = S[current][i][j] + (sum_local(i, j) * dt);
+                    if(test)
+                        S[next][i][j] += sum_local_test(i, j, t) * dt;
                 }
             }
 
         }
+
+        next = nt % 2;
+
+        //testing the code for correctness
+        if(test)
+            for(long i = 0; i < nx; ++i)
+                for(long j = 0; j < ny; ++j)
+                    error += (S[next][i][j] - w(i, j, nt)) * (S[next][i][j] - w(i, j, nt));
 
         // Return the solution at time-step 'nt'.
         return S[nt % 2];
     }
 };
 
+int batch_tester()
+{
+    std::uint64_t nx, ny, nt, eps, num_tests;
+    bool test_failed = 0;
+    
+    std::cin >> num_tests;
+    for(long i = 0; i < num_tests; ++i)
+    {
+        std::cin >> nx >> ny >> nt >> eps >> k >> dt >> dh;
+        
+        // Create the solver object
+        solver solve(nx, ny, nt, eps);
+        solve.test_init();
+        
+        solve.do_work();
+        
+        if (solve.error / (double)(nx * ny) > 1e-6)
+        {
+            test_failed = 1;
+            break;
+        }
+    }
+
+    // output regular expression whether the test passed or failed
+    if(test_failed)
+        std::cout << "Tests Failed" << std::endl;
+    else
+        std::cout << "Tests Passed" << std::endl;
+
+    return hpx::finalize();
+}
 
 int hpx_main(hpx::program_options::variables_map& vm)
 {
@@ -164,18 +272,18 @@ int hpx_main(hpx::program_options::variables_map& vm)
     if (vm.count("no-header"))
         header = false;
 
+    //batch testing for ctesting
+    if (vm.count("test_batch"))
+        return batch_tester();
+
     // Create the solver object
     solver solve(nx, ny, nt, eps);
 
     //Take inputs from stdin for testing
     if(vm.count("test"))
-    {
-        solve.init_space();
-    }
+        solve.test_init();
     else
-    {
-        solve.default_init();
-    }
+        solve.input_init();
 
     // Measure execution time.
     std::uint64_t t = hpx::util::high_resolution_clock::now();
@@ -183,9 +291,15 @@ int hpx_main(hpx::program_options::variables_map& vm)
     // Execute nt time steps on nx grid points.
     solver::space solution = solve.do_work();
 
+    std::uint64_t elapsed = hpx::util::high_resolution_clock::now() - t;
+
+    //print the calculated error
+    //print comparison only when the 'cmp' flag is set
+    if(vm.count("test"))
+        solve.print_error(vm["cmp"].as<bool>());
+
     // Print the final solution
     if (vm.count("results"))
-    {
         for (long i = 0; i < nx; ++i)
         {
             for (long j = 0; j < ny; ++j)
@@ -194,9 +308,6 @@ int hpx_main(hpx::program_options::variables_map& vm)
             }
             std::cout << std::endl;
         }
-    }
-
-    std::uint64_t elapsed = hpx::util::high_resolution_clock::now() - t;
 
     std::uint64_t const os_thread_count = hpx::get_os_thread_count();
     print_time_results(os_thread_count, elapsed, nx, ny, nt, header);
@@ -210,20 +321,25 @@ int main(int argc, char* argv[])
 
     po::options_description desc_commandline;
     desc_commandline.add_options()
-        ("test", "use arguments from stdin for testing (default: false)")
+        ("test", "use arguments from numerical solution for testing (default: false)")
+        ("test_batch", "test the solution against numerical solution against batch inputs (default: false)")
         ("results", "print generated results (default: false)")
-        ("nx", po::value<std::uint64_t>()->default_value(10),
+        ("cmp", po::value<bool>()->default_value(true),
+         "Print expected versus actual outputs")
+        ("nx", po::value<std::uint64_t>()->default_value(50),
          "Local x dimension")
-        ("ny", po::value<std::uint64_t>()->default_value(10),
+        ("ny", po::value<std::uint64_t>()->default_value(50),
         "Local y dimension")
         ("nt", po::value<std::uint64_t>()->default_value(45),
          "Number of time steps")
         ("eps", po::value<std::uint64_t>()->default_value(5),
          "Epsilon for nonlocal equation")
-        ("dt", po::value<double>(&dt)->default_value(1.0),
+        ("k", po::value<double>(&k)->default_value(1),
+         "Heat transfer coefficient (default: 0.5)")
+        ("dt", po::value<double>(&dt)->default_value(0.0005),
          "Timestep unit (default: 1.0[s])")
-        ("dh", po::value<double>(&dh)->default_value(1.0),
-         "quantization across space")
+        ("dh", po::value<double>(&dh)->default_value(0.02),
+         "Quantization across space")
         ( "no-header", "do not print out the csv header row")
     ;
 
